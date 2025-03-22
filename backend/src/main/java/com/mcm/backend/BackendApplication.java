374cadc9ee3dddaeaeefe3627403d6393c174e4c
Yan Zan <zanyan2004@163.com>
2025-03-22 19:51:57 +0800

    4.1 实现了一个能通过jwt验证的网站模板

--- backend/src/main/java/com/mcm/backend/BackendApplication.java
+++ backend/src/main/java/com/mcm/backend/BackendApplication.java
@@ -2,12 +2,16 @@ package com.mcm.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+//        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        System.out.println(passwordEncoder.encode("6762"));
     }
 
 }
