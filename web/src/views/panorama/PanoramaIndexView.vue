7ccc93182ee283fd4d214fda048ef77829571629
Yan Zan <zanyan2004@163.com>
2025-04-14 17:12:55 +0800

    9.1.3 添加了导航栏等组件

--- web/src/views/panorama/PanoramaIndexView.vue
+++ web/src/views/panorama/PanoramaIndexView.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="panorama-containner d-flex justify-content-center align-items-center min-vh-100">
-        <div id="panorama"></div>
+        <div id="panorama">
+            <div id="controls">
+                <div class="ctrl" id="pan-up">&#9650;</div>
+                <div class="ctrl" id="pan-down">&#9660;</div>
+                <div class="ctrl" id="pan-left">&#9664;</div>
+                <div class="ctrl" id="pan-right">&#9654;</div>
+                <div class="ctrl" id="zoom-in">&plus;</div>
+                <div class="ctrl" id="zoom-out">&minus;</div>
+                <div class="ctrl" id="fullscreen">&#x2922;</div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,9 +20,42 @@ import { onMounted } from 'vue';
 export default{
     setup(){
         const initPannellum = () => {
-            window.pannellum.viewer('panorama', {
+            const viewer = window.pannellum.viewer('panorama', {
                 type: 'equirectangular',
-                panorama: require('@/assets/panorama/sphere.jpg')
+                panorama: require('@/assets/panorama/sphere.jpg'),
+                "autoLoad": true, // 自动加载
+                "autoRotate": -2, // 自动旋转：每秒顺时针旋转2°
+                // "preview": "@/assets/images/shiroha.jpg", // 设置预览图片
+                "title": "山谷", // 设置标题
+                "author": "Yan Zan", // 设置作者
+                // "northOffset": 247.5, // 设置指南针
+                // "pitch": 2.3, // 设置初始视图
+                // "yaw": -135.4, // 同上
+                // "hfov": 120, // 同上
+                "showControls": false,
+            });
+
+            // Make buttons work
+            document.getElementById('pan-up').addEventListener('click', function () {
+                viewer.setPitch(viewer.getPitch() + 10);
+            });
+            document.getElementById('pan-down').addEventListener('click', function () {
+                viewer.setPitch(viewer.getPitch() - 10);
+            });
+            document.getElementById('pan-left').addEventListener('click', function () {
+                viewer.setYaw(viewer.getYaw() - 10);
+            });
+            document.getElementById('pan-right').addEventListener('click', function () {
+                viewer.setYaw(viewer.getYaw() + 10);
+            });
+            document.getElementById('zoom-in').addEventListener('click', function () {
+                viewer.setHfov(viewer.getHfov() - 10);
+            });
+            document.getElementById('zoom-out').addEventListener('click', function () {
+                viewer.setHfov(viewer.getHfov() + 10);
+            });
+            document.getElementById('fullscreen').addEventListener('click', function () {
+                viewer.toggleFullscreen();
             });
         }
         onMounted(() => {
@@ -34,5 +77,28 @@ export default{
 #panorama {
     width: 1200px;
     height: 800px;
+}
+#controls {
+    position: absolute;
+    bottom: 0;
+    z-index: 2;
+    text-align: center;
+    width: 100%;
+    padding-bottom: 3px;
+    pointer-events: none;
+}
+
+.ctrl {
+    padding: 8px 5px;
+    width: 30px;
+    text-align: center;
+    background: rgba(200, 200, 200, 0.8);
+    display: inline-block;
+    cursor: pointer;
+    pointer-events: auto;
+}
+
+.ctrl:hover {
+    background: rgba(200, 200, 200, 1);
 }
 </style>
